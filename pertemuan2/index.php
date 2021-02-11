a81fe6a48efaa0965b48ab92abcc728ef4c04dfd
Marwan2310 <imaroneu@gmail.com>
2021-02-11 21:04:20 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,62 @@
+<? 
+/*
+Marwan Hakim
+203040121
+https://github.com/Marwan2310/pw2021_203040121
+Pertemuan 2 - 11 Februari 2021
+Mempelajari mengenai sintanks PHP
+*/
+?>
+
+<?php
+// Pertemuan 2 - PHP Dasar 
+// Sintaks PHP
+
+// Standar Output
+// echo, print 
+// print_r 
+// var_dump
+
+// penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Marwan Hakim";
+// echo 'Halo, nama saya $nama';
+
+// Operator 
+// aritmatika
+// + - * / %
+
+// penggabung string / concatenation / concat
+// .
+// $nama_depan = "Marwan";
+// $nama_belakang = "Hakim";
+// echo $nama_depan . " " .$nama_belakang;
+
+// Assigment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+
+// $nama = "Marwan";
+// $nama .= " ";
+// $nama .= "Hakim";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, == !=
+// var_dump(1 == "1");
+
+// indentitas
+// ===, !===
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, ! 
+$x = 30;
+var_dump($x < 20 || $x % 2 == 0);
