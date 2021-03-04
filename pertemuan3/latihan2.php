eacf5a7106bf601cb9bb8be8d64cd597cc43ea67
Marwan2310 <imaroneu@gmail.com>
2021-03-04 19:32:45 +0700

    menambahkan nama pada video pertemuan3

--- pertemuan3/latihan2.php
+++ pertemuan3/latihan2.php
@@ -1,3 +1,13 @@
+<?php 
+/*
+Marwan Hakim
+203040121
+https://github.com/Marwan2310/pw2021_203040121
+Pertemuan 3 - 19 Februari 2021
+Mempelajari mengenai Struktur kendali PHP
+*/
+?>
+
 <?php
 // Pengkondisian / Percabangan
 // if else
