153e237064aa677d7f64327b698e60f8ae010357
Marwan2310 <imaroneu@gmail.com>
2021-04-13 21:04:35 +0700

    tugas praktikum 5

---
+++ praktikum/tugas5/Latihan5a/php/functions.php
@@ -0,0 +1,41 @@
+<?php 
+/*
+Nama    :Marwan Hakim
+NRP     :203040121
+Shift   :Jumat 13.00
+Tugas   : Tugas 5 Praktikum Pemrograman Web
+*/
+?>
+<?php 
+// fungsi untuk memlakukan koneksi ke database
+
+function koneksi() 
+{
+    $conn = mysqli_connect("localhost", "root", "" );
+    mysqli_select_db( $conn, "pw_tubes_203040121");
+    
+
+    return $conn;
+
+
+}
+
+// function untuk mealukan query dan memasukannya kedalam array
+
+function query($sql)
+{
+    $conn = koneksi();
+    $result = mysqli_query($conn, "$sql");
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+   return $rows; 
+
+
+
+}
+
+
+
+?>
